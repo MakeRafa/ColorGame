a583d2b52a493452516f133bb3da6f91a26e2352
Rafa <rafavazquez229@gmail.com>
2020-11-28 13:45:35 -0800

    Initial Commit

---
+++ ColorGame/ColorGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorGameApp.swift
+//  ColorGame
+//
+//  Created by Rafael Vazquez on 11/28/20.
+//
+
+import SwiftUI
+
+@main
+struct ColorGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
